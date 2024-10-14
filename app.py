f416b851d3a63a1ab9748b78ebc571b4104f97cc
Rudra Gowda H <121507406+Rudragowdah@users.noreply.github.com>
2024-10-14 13:37:23 +0530

    Updated ToDoApp

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template, request
+from flask import Flask , render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 app = Flask(__name__)
@@ -10,20 +10,39 @@ def hello_world():
     if request.method=='POST':
         # print('POST')
         title = request.form['title']
+        print(title)
         desc = request.form['desc']
-    todo = ToDo(title=title,desc=desc)
-    db.session.add(todo)
-    db.session.commit()
+        todo = ToDo(title=title,desc=desc)
+        db.session.add(todo)
+        db.session.commit()
     allTodo = ToDo.query.all()
     # print(allTodo)
     # return 'Hello, World!'
     return render_template('index.html',allTodo=allTodo)
 
-@app.route('/show')
-def products():
-    allTodo = ToDo.query.all()
-    print(allTodo)
-    return 'Products'
+@app.route('/delete/<int:SNo>')
+def delete(SNo):
+    todo = ToDo.query.filter_by(SNo=SNo).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect('/')
+
+@app.route('/update/<int:SNo>',methods=['GET','POST'])
+def update(SNo):
+    if request.method=='POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = ToDo.query.filter_by(SNo=SNo).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect('/')
+
+    todo = ToDo.query.filter_by(SNo=SNo).first()
+    return render_template('update.html',todo=todo)
+
+
 
 class ToDo(db.Model):
     SNo = db.Column(db.Integer,primary_key=True)
